2abbcf82763bcbb4fe73b25d2f95e49a79a91b1a
Castelli87 <davidecastelli87@hotmail.com>
2024-01-13 00:07:58 +0000

    TEST lint

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -12,12 +12,9 @@ class ModelTest(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is succeful"""
-        email='test@example.com'
-        password='testpass123'
-        user=get_user_model().objects.create_user(
-            email=email,
-            password=password
-        )
+        email = "test@example.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(email=email, password=password)
 
-        self.assertEqual(user.email,email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
